0c19c39fc9157fcefda4bf0c6051cc215f9fa2d7
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 06:22:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,7 +37,16 @@ def game_hash
         :blocks => 8,
         :slam_dunks => 5
         },
-        "Jason Terry" =>
+        "Jason Terry" => {
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
+      }
         
       }
     },
