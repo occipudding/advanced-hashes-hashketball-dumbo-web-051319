f67e21a603f62a574eef61cb694e7eddf19a86de
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 06:12:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,10 +61,11 @@ def num_points_scored(name)
   val = 0
   game_hash.keys.collect do |loc|
     if game_hash[loc][:players].include?(name)
-      game_hash[loc][:players][name][:points]
+      val = game_hash[loc][:players][name][:points]
     end
     #binding.pry
   end
+  val
 end
 
 num_points_scored("Alan Anderson")
