e46b242c9522ea9bdcc0348b59454b4eb2cb538e
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 06:18:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,7 +25,8 @@ def game_hash
         :steals => 12,
         :blocks => 12,
         :slam_dunks => 7
-        }
+        },
+        
       }
     },
     :away => {
@@ -44,14 +45,25 @@ def game_hash
         },
         "Bismack Biyombo" => {
           :number => 0,
-        :shoe => 16,
-        :points => 12,
-        :rebounds => 4,
-        :assists => 7,
-        :steals => 7,
-        :blocks => 15,
-        :slam_dunks => 10
-        }
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10
+        },
+        "DeSagna Diop" => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+        },
+        
       }
     }
   }
