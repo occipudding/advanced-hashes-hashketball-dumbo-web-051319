22fdd40e97e92ed0b21cc6b34cc741a17046fb04
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 05:08:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -58,9 +58,9 @@ def game_hash
 end
 
 def num_points_scored(name)
-  (1..2).each do |i|
+  (0..1).each do |i|
     if game_hash[game_hash.keys[i]][:players].include?(name)
-      return game_hash[game_hash.keys[i]][:players][name][:points]
+      return game_hash[game_hash.keys[i]][:players][name][:points].to_i
     end
   end
 end
